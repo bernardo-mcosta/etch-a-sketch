467c74a628b314e1fec458e969c664c9fc6811ef
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2023-01-03 22:47:08 -0300

    Add buttons for different modes

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-const grid = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 const gridSize = 16; // this parameter will change dynamically later
 const gridHeight = 500; // in pixels
 const gridWidth = 500;
 
-function createGrid(gridSize) {
-  grid.style.display = "grid";
-  grid.style.height = `${gridHeight}px`;
-  grid.style.width = `${gridWidth}px`;
-  grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
-  grid.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+grid.style.display = "grid";
+grid.style.height = `${gridHeight}px`;
+grid.style.width = `${gridWidth}px`;
+grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+grid.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
 
-  for (i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div);
-  }
+for (i = 0; i < gridSize * gridSize; i++) {
+  const div = document.createElement("div");
+  div.classList.add("square");
+  grid.appendChild(div);
 }
 
-createGrid(gridSize);
-
+const squares = document.querySelectorAll(".square");
 let color = "black";
 
+const buttonEraser = document.querySelector(".button-eraser");
+buttonEraser.addEventListener("click", () => {
+  color = "white";
+});
+
+const buttonBlack = document.querySelector(".button-black");
+buttonBlack.addEventListener("click", () => {
+  color = "black";
+});
+
+const buttonClear = document.querySelector(".button-clear");
+buttonClear.addEventListener("click", () => {
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
+
+const buttonRainbow = document.querySelector(".button-rainbow");
+buttonRainbow.addEventListener("click", () => {
+  color = getRandomColor();
+});
+
 // This three event listeners will allow the coloring effect to happen only when the mouse is both being pressed and dragged
-const squares = document.querySelectorAll(".square");
+
 let isDragging = false;
 squares.forEach((square) => {
   square.addEventListener("mousedown", () => {
@@ -36,4 +55,16 @@ squares.forEach((square) => {
   square.addEventListener("mouseup", () => {
     isDragging = false;
   });
+  square.addEventListener("dragstart", (event) => {
+    event.preventDefault;
+  });
 });
+
+////
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
